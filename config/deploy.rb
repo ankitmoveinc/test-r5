d92adb7e9de55470a30daf6707dca5d14fddf4d8
Ankit Gupta <ankit.gupta@move.com>
2016-01-01 22:21:50 -0800

    some more config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'seller_leads'
+set :repo_url, 'https://github.com/ankit8898/test-r5.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/opt/railssvc/rdc-next'
 
 # Default value for :scm is :git
 # set :scm, :git
