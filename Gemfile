143a1d2d503a087a0c45cbb92878d8424ed098d2
Ankit Gupta <ankit.gupta@move.com>
2016-01-02 19:36:25 -0800

    some gem updates

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'puma'
 
 gem 'passenger'
+gem 'capistrano-rvm'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/deploy.rb
+++ config/deploy.rb
@@ -3,13 +3,23 @@ lock '3.4.0'
 
 set :application, 'seller_leads'
 set :repo_url, 'git@github.com:ankitmoveinc/test-r5.git'
-
+#set :user, 'railssvc'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/opt/railssvc/rdc-next'
 
+set :ruby_version, '2.3.0'
+set :user, 'railssvc'
+set :rvm_type, :system
+set :rvm_ruby_version, "#{fetch(:ruby_version)}@#{fetch(:application)}"
+# set :ssh_options, {
+#   forward_agent: true,
+#   user: fetch(:user),
+#   keys: 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAqDk+fj0QpXV1WPkV5OlTuhep70LZjhciYpEdjGqvmaltEenuAj8epyZSJQZqfnwwj4cE8ZvLzJ1sTHKnVsP+PQYAEXB7mLBomG+pum53oforvUEYQkLsMUpxSpqh3frYcR1yyIb+Qm65KCOo9pQbWe43LSkUDfWcYOYKyjvn42z+qBYe2PQ8QjUHS5k2lBmRCEbbjP3dfWBAQMoo4czMxpZWd/O9HKJ6I6vCsR/bYsMWfTqigPY53VhB8zpKMKwcssHt4OLh54GTjdT3B42kvMs8M4A9MGiqMkMeUhN78pr/pTH+GzvV9vDUTP5zVgZfD6XE8tslyhyGKavZCD71JQ== itops-sysadmin@move.com
+# ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAw7bA3ylxEkOUYWNCZuJg+HIVPySHo2slrTyOuGnwSlMdZ307eN4hCdMIcH59EI67+848zGPOTzjH0fBXAZa6bW/d6MbLsqZXcE5HqrUE4eSCbqvl8h8mv74ysHmhx/llDEQeWLZ6C/f0S7EaQMxMqqo5jKwGYJyzy/t2jtHREPguh3Q8JoZUmW/90ui/cZ13ZpVJsYoOPRocd4Ua6t1k+GCGDfO0Pigp6apaS96ygPBO7FFNZXZbNosdsnKehZffiRT4Y7zqkpD7Hfj4iY6iJz3CNEXqU6bBUDmN8Gyr7VX1Wqy32pAKK6SCVIsYxvPgFjrQODYpF2YQ2ZjWbj5DgQ=='
+# }
 # Default value for :scm is :git
 # set :scm, :git
 
@@ -32,17 +42,27 @@ set :deploy_to, '/opt/railssvc/rdc-next'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 3
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc "Run bundle install"
+  task :run_bundle_install do
+    on roles(:all) do
+      within current_path do
+        execute :bundle, 'install --local'
+        #execute 'cd /opt/railssvc/rdc-next/current && bundle install --local'
+      end
     end
   end
 
+  desc "Hard reset passenger"
+  task :hard_reset do
+    on roles(:all) do
+      execute "sudo /etc/init.d/passenger stop || true && sudo /etc/init.d/passenger start"
+    end
+  end
+
+  after :published, :run_bundle_install
+  after :finished, :hard_reset, :clear_cache
 end
